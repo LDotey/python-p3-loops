88e0f1126090294ae2e66068e72084ac5fbb503e
Leigh Dotey <leighdotey@gmail.com>
2024-08-06 15:26:30 -0400

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    num = 10
+    while num > 0:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    return [x**2 for x in int_list]
+    
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):
+        if num % 15 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
